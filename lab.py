3c6bc2df8bef085774cbe195b60051406510578f
Chad Ruble <cruble4@gmail.com>
2018-10-30 16:04:58 -0400

    complete

---
+++ lab.py
@@ -0,0 +1,26 @@
+from car import Car
+from person import Person 
+
+
+pam = Person("Pam Beasley", "Secretary")
+# chrysler = Car("Chrysler", "Sebring Convertible", 2004, "Michael Scott")
+
+datsun = Car("Datsun", "280Z", 1978, {'name': "Dwight Schrute", 'occupation': "Paper Salesperson", 'favotite_tv_show': "Battlestar Galactica"})
+toyota = Car("Toyota", "Yaris", 2007, pam)
+
+
+michael = Person("Michael Scott", "Regional Manager")
+sebring = Car("Chrysler", "Sebring Convertible", 2004, michael)
+dwight = Person("Dwight Schrute", "Paper Salesperson")
+datsun.owner = dwight
+# let's create more people
+meredith = Person("Meredith Palmer", "Purchaser - Supplier Relations")
+ford_minivan = Car("Ford", "Aerostar Minivan", 1997, meredith)
+jim = Person("Jim Halpert", "Paper Salesperson")
+corolla = Car("Toyota", "Corolla", 2000, jim)
+stanley = Person("Stanley Hudson", "Paper Salesperson")
+camry = Car ("Toyota", "Camry", 2008, stanley)
+
+
+for car in Car._all: 
+	print(vars(car))
--- person.py
+++ person.py
@@ -6,3 +6,47 @@ class Person:
     def __init__(self, name, occupation):
         self._name = name
         self._occupation = occupation
+
+    @property
+    def name(self):
+    	return self._name
+    
+    @property
+    def occupation(self):
+    	return self._occupation
+
+    @classmethod
+    def has_oldest_car(cls):
+    	car_year = {}
+    	for car in Car._all: 
+    		car_year[car] = car.year
+    	return min(car_year, key=car_year.get)
+
+    @classmethod
+    def drives_a(cls, make):
+    	owners = []
+    	for car in Car._all: 
+    		if car.make == make: 
+    			owners.append(car.owner)
+    	return owners 
+
+    def drives_same_make_as_me(self):
+    	owners = []
+    	my_car = filter(lambda car: car.owner == self, Car._all)[0]
+    	same_makes = filter(lambda car: car.make == my_car.make and car.owner != self, Car._all)
+    	return list(map(lambda car: car.owner, same_makes))
+
+    	# for car in Car._all: 
+    	# 	if car.make == self.
+ 
+    	
+
+
+
+
+    	# find all cars with dates
+    	# find lowest date
+    	# owner from that car 
+
+    
+
